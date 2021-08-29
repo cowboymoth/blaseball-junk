2805dfd50794fd49f82748a0aeb7a187449cd2f8
cowboymoth <73248302+cowboymoth@users.noreply.github.com>
2021-08-28 19:37:09 -0500

    Update before-performance-stats.sql

--- before-performance-stats.sql
+++ before-performance-stats.sql
@@ -9,9 +9,10 @@ with batting_events_plus AS(
 		SUM(at_bat) AS at_bats,
 		SUM(home_run) AS home_runs,
 		SUM(runs_batted_in) AS rbis
+		
 		FROM data.batting_stats_all_events
-WHERE season=19
-AND day<50
+WHERE season=19 -- EDIT HERE! remember to zero index ; season 1=0, season 2=1, etc.
+AND day<50 -- EDIT HERE! zero-indexed
 AND is_haunting=0
 GROUP BY player_id, batter_team_id )
 
