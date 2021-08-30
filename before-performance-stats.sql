ca3a3a1b7a9c2d6e8fd77976f13f106e97cb9f38
cowboymoth <73248302+cowboymoth@users.noreply.github.com>
2021-08-30 00:27:37 -0500

    Update before-performance-stats.sql

--- before-performance-stats.sql
+++ before-performance-stats.sql
@@ -1,6 +1,3 @@
-
-WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP
-
 with batting_events_plus AS(
 	SELECT 	
 		pl.player_name, 
@@ -12,20 +9,30 @@ with batting_events_plus AS(
 		SUM(b.walk) AS walks,
 		SUM(b.hbp) AS hbp,
 		SUM(b.sacrifice_fly) AS sac_flys,
-		SUM(b.total_bases) AS total_bases,
-		SUM(r.was_base_stolen) AS stolen_bases,
-		SUM(r.was_caught_stealing) AS caught_stealing
+		SUM(b.total_bases) AS total_bases
 		
 	FROM data.batting_stats_all_events b
-	INNER JOIN data.running_stats_all_events r ON b.player_id = r.player_id
 	INNER JOIN data.players pl ON b.player_id = pl.player_id AND pl.valid_until is NULL
 	INNER JOIN data.teams te ON te.team_id = b.batter_team_id AND te.valid_until is NULL
-WHERE b.season=6 -- EDIT HERE! remember to zero index ; season 1=0, season 2=1, etc.
-AND b.day<50 -- EDIT HERE! also zero-indexed
+--------------------------------------------------------------------------------------------
+-- EDIT HERE! Remember season and day are zero indexed so season 1=0, season 2=1, etc.
+WHERE b.season=6
+AND b.day<=32
 AND is_haunting=0 
-GROUP BY pl.player_name, te.nickname )
-
-SELECT * FROM
-batting_events_plus bep
-
-WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP
+GROUP BY pl.player_name, te.nickname 
+ORDER BY te.nickname, pl.player_name ),
+--------------------------------------------------------------------------------------------
+player_stats AS (
+	SELECT
+		player_name,
+		nickname,
+		walks,
+		hits,
+		home_runs,
+		rbis,
+		ROUND((hits*1./at_bats),3) AS batting_average,
+		ROUND(((hits+walks+hbp)*1./(at_bats+walks+hbp)),3) AS on_base_pct,
+		ROUND((total_bases*1./at_bats),3) AS slugging_pct
+	FROM batting_events_plus bep)
+SELECT * FROM 
+player_stats
